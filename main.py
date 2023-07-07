3e8cd60f54d0a160d416bb9364bfd4b3a49f9a92
Ruud van den Boomen <ruud.van.den.boomen@sendcloud.com>
2023-07-07 16:48:59 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+# Serve the index.html file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Handle WebSocket connections
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+  
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('play_audio')
+def handle_play_audio():
+    print('Playing audio')
+    # Trigger audio playback on the client side
+    socketio.emit('play_audio')
+
+@socketio.on('play_audio_2')
+def handle_play_audio_2():
+    print('Playing audio')
+    # Trigger audio playback on the client side
+    socketio.emit('play_audio_2')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', debug=True,  use_reloader=True)
