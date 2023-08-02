7fbd561e326adbbddbbd82fd0a7733dcfee64201
Ruud van den Boomen <ruud.van.den.boomen@sendcloud.com>
2023-08-02 18:43:15 +0200

    add simple non persitent chat functionality

--- api/main.py
+++ api/main.py
@@ -1,14 +1,18 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from flask_socketio import SocketIO
+import re
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+online_users = set()
+regex_pattern = re.compile(r"(?i)\bok\b.*\b(lets|let's)\b .*\bgo\b")
 
 
 # Serve the index.html file
@@ -20,12 +24,21 @@ def index():
 # Handle WebSocket connections
 @socketio.on("connect")
 def handle_connect():
-    print("Client connected")
+    online_users.add(request.sid)
+    socketio.emit("update_online_count", {"count": len(online_users)})
 
 
 @socketio.on("disconnect")
 def handle_disconnect():
-    print("Client disconnected")
+    online_users.discard(request.sid)
+    socketio.emit("update_online_count", {"count": len(online_users)})
+
+
+@socketio.on("message")
+def handle_message(data):
+    message = data["message"]
+    if regex_pattern.match(message):
+        socketio.emit("message", {"message": message})
 
 
 @socketio.on("play_audio")
