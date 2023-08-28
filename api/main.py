47c824eea3b2eb3db964f57e0d2a44cb63ad09fb
Ruud van den Boomen <ruud.van.den.boomen@sendcloud.com>
2023-08-28 19:42:26 +0200

    add service worker with pushManager push notifications

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
 import os
+import re
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, jsonify, render_template, request, send_from_directory, abort
 from flask_socketio import SocketIO
-import re
+from pywebpush import WebPushException, webpush
 
 load_dotenv()
 
@@ -11,9 +12,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-online_users = dict()
 regex_pattern = re.compile(r"ok.*(lets|let's).*go")
+online_users = dict()
 messages: list = []
+subscriptions: dict = {}
+
+# For push notifications
+PUBLIC_KEY = os.getenv("PUBLIC_KEY")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 
 
 # Serve the index.html file
@@ -28,8 +34,41 @@ def handle_disconnect():
     socketio.emit("update_online_count", {"count": len(online_users)})
 
 
+@app.route("/<user_id>/subscribed", methods=["GET"])
+def subscribed_check(user_id):
+    if user_id in subscriptions:
+        return jsonify({"subscribed": True})
+    return jsonify({"subscribed": False})
+
+
+@app.route("/<user_id>/subscribe", methods=["PUT"])
+def subscribe(user_id):
+    subscription = request.get_json()
+
+    if user_id:
+        # Store the subscription details
+        subscriptions[user_id] = subscription
+        return jsonify({"message": "Subscribed successfully"})
+    else:
+        return jsonify({"error": "User ID not provided"})
+
+
+@app.route("/<user_id>/unsubscribe", methods=["PUT"])
+def unsubscribe(user_id):
+    if user_id:
+        try:
+            # Remove the subscription details
+            subscriptions.pop(user_id)
+        except KeyError:
+            abort(400, description="Bad request: invalid User ID.")
+        return jsonify({"message": "Unsubscribed successfully"})
+    else:
+        return jsonify({"error": "User ID not provided"})
+
+
 @socketio.on("message")
 def handle_message(data):
+    sender_user_id = data["user_id"]
     message = data["message"]
     if regex_pattern.match(message):
         name = online_users.get(request.sid, "")
@@ -39,6 +78,20 @@ def handle_message(data):
         }
         socketio.emit("message", {"message": message})
         messages.append(message)
+
+        for user_id, subscription in subscriptions.items():
+            if user_id != sender_user_id:
+                try:
+                    webpush(
+                        subscription_info=subscription,
+                        data=message["content"],
+                        vapid_private_key=PRIVATE_KEY,
+                        vapid_claims={"sub": "mailto:ruudvdboomen@hotmail.com"},
+                    )
+                except WebPushException as e:
+                    print("Error sending notification:", e)
+
+        return jsonify({"message": "Notifications sent successfully"})
 
 
 @socketio.on("set_name")
---
+++ api/static/scripts/service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('push', function (event) {
+    const options = {
+        body: event.data.text(),
+        icon: 'https://ok-lets-go.ruudvandenboomen.nl//static/images/ok-lets-go.png',
+        badge: 'https://ok-lets-go.ruudvandenboomen.nl//static/images/ok-lets-go.png',
+        data: {
+            url: 'https://ok-lets-go.ruudvandenboomen.nl/'
+        }
+    };
+    console.log(event)
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+
+    const clickedUrl = event.notification.data.url;
+
+    if (clickedUrl && clients.openWindow) {
+        event.waitUntil(
+            clients.openWindow(clickedUrl)
+        );
+    }
+});
