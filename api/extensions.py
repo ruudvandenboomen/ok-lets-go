f25ab948083aeed7c16b50e0f68e0cf41d7187af
Ruud van den Boomen <ruud.van.den.boomen@sendcloud.com>
2023-11-04 10:39:58 +0100

    fix flask_sqlalchemy ssl error

--- api/extensions.py
+++ api/extensions.py
@@ -1,5 +1,13 @@
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+
+
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        options = super().apply_pool_defaults(app, options)
+        options["pool_pre_ping"] = True
+        return options
+
 
 db = SQLAlchemy()
 migrate = Migrate()
